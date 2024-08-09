220b98485055b4f0edb3ca640656cdfe5b8b9501
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-08-09 14:35:02 -0300

    commit: Interface Consumer

---
+++ src/com/java/FunctionalInterface/ConsumerExample.java
@@ -0,0 +1,27 @@
+package com.java.FunctionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        //Lista de inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        //Criando consumer
+//        Consumer<Integer> imprimirNumeroPar = numero -> {
+//            if (numero % 2 == 0){
+//                System.out.println(numero);
+//            }
+//        };
+
+        //Criando o consumer dentro do stream e usando para imprimir com lambda
+        numeros.stream().forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        });
+    }
+
+}
