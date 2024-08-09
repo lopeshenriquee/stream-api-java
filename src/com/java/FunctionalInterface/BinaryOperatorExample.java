899a5d9003088c894ae6817ce554b6fc2bb1b4ed
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-08-09 15:49:00 -0300

    commit: BinaryOperator Function

---
+++ src/com/java/FunctionalInterface/BinaryOperatorExample.java
@@ -0,0 +1,18 @@
+package com.java.FunctionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        BinaryOperator<Integer> somar = (n1, n2) -> n1 + n2;
+
+        int resultado = numeros.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println("A soma dos numeros é: " + resultado);
+    }
+}
